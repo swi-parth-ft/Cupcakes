e5155c5a5e68cf28468368ee08df6de6485b450d
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2023-02-14 16:24:39 -0500

    Initial Commit

---
+++ Cupcakes/CupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesApp.swift
+//  Cupcakes
+//
+//  Created by Parth Antala on 2023-02-14.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
